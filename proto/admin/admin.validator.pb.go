7361e20e7994fac7cb6c16cda7217dd73c0335a4
贺绍锋 <heshaofeng@shizhuang-inc.com>
2023-01-05 01:19:45 +0800

    proto

--- proto/admin/admin.validator.pb.go
+++ proto/admin/admin.validator.pb.go
@@ -7,10 +7,10 @@ import (
 	fmt "fmt"
 	math "math"
 	proto "github.com/golang/protobuf/proto"
-	_ "google.golang.org/protobuf/types/known/timestamppb"
 	_ "github.com/gogo/protobuf/gogoproto"
 	_ "github.com/mwitkow/go-proto-validators"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
+	_ "google.golang.org/protobuf/types/known/timestamppb"
 	time "time"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
